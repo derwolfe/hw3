f76a93f5c5b3a088b353f98b88ef48aff407da1c
derwolfe <chriswwolfe@gmail.com>
2011-09-21 18:40:46 -0500

    fixed indent problems w/ diff tab stops

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -75,9 +75,9 @@ void Double_list::d_list ( const d_list& a_list ) : size(a_list.size)
  */
 void Double_list::~d_list () 
 {
-    while ( !is_empty ) {
-       remove ( 0 );
-    }    
+  while ( !is_empty ) {
+    remove ( 0 );
+  }    
 }
 
 /* 
@@ -87,12 +87,11 @@ void Double_list::~d_list ()
  */
 bool Double_list::is_empty () const 
 {
-    if ( size == 0 ) {
-        assert ( head == NULL );
- /*       assert ( tail == NULL ); --removed from header file*/
-        return true;
-    }
-    return false;
+  if ( size == 0 ) {
+    assert ( head == NULL );
+    return true;
+  }
+  return false;
 }
 
 /*
@@ -100,75 +99,75 @@ bool Double_list::is_empty () const
  */
 int Double_list::get_length () const
 {
-    return size;
+  return size;
 }
 
 
 bool Double_list::insert ( int index, list_item_type new_item )
 {
-    int new_length = get_length() + 1;
-    if (( index < 1 ) || ( index > new_length )) {
-        throw list_index_out_of_range_exception(
-                "List Index Out of Range Exception: the index you provided is out of range");
+  int new_length = get_length() + 1;
+  if (( index < 1 ) || ( index > new_length )) {
+      throw list_index_out_of_range_exception(
+              "List Index Out of Range Exception: the index you provided is out of range");
+  } else {
+    /* 
+     * Normal execution of the subroutine, initialiases a new_ptr for the NEW
+     * double node
+     */
+    Double_node *new_ptr = new Double_node;
+    if ( new_ptr == NULL ) {
+      throw list_exception("No more memory for new node");
     } else {
-        /* 
-         * Normal execution of the subroutine, initialiases a new_ptr for the NEW
-         * double node
+      size = new_length;
+      new_ptr->item = new_item;
+      /* 
+       * Insert to the head of the list, set the negative values NULL as they
+       * are out of bound/
+       */
+      if ( index == 0 ) {
+        new_ptr->next = head;
+        new_ptr->prev = NULL;
+        /*
+         * If the insertion is occuring in a list that isn't empty
+         * do the following, the following initializes the 2nd element
+         * in the list
          */
-        Double_node *new_ptr = new Double_node;
-        if ( new_ptr == NULL ) {
-            throw list_exception("No more memory for new node");
-        } else {
-            size = new_length;
-            new_ptr->item = new_item;
-            /* 
-             * Insert to the head of the list, set the negative values NULL as they
-             * are out of bound/
-             */
-            if ( index == 0 ) {
-                new_ptr->next = head;
-                new_ptr->prev = NULL;
-                /*
-                 * If the insertion is occuring in a list that isn't empty
-                 * do the following, the following initializes the 2nd element
-                 * in the list
-                 */
-                if ( head != NULL ) {
-                /* 
-                 *  based on the order of the dereferences, 
-                 *  (new_ptr->next)->prev sets the next
-                 *  element's 'prev' pointer to the newly minted node
-                 */
-                    new_ptr->next->prev = new_ptr;
-                }
-                head = new_ptr;
-            /* 
-             * Now we get to see what happens if there are elements
-             * in the list. cur will point directly to the item located
-             * behind the index. 
-             */
-            } else {
-                /* three steps, first set new_ptr's next to the current item,
-                 * this way no information is destroyed but a reference is produced
-                 * second, set the next pointer of the item  previously preceding cur
-                 * to point to the new item
-                 * third set curs previous pointer to the new item.
-                 */
-                Double_node *cur = find ( index );
-                new_ptr->next = cur;
-                cur->prev->next = new_ptr;
-                cur->prev = new_ptr;
-            }
+        if ( head != NULL ) {
+        /* 
+         *  based on the order of the dereferences, 
+         *  (new_ptr->next)->prev sets the next
+         *  element's 'prev' pointer to the newly minted node
+         */
+            new_ptr->next->prev = new_ptr;
         }
+        head = new_ptr;
+    /* 
+     * Now we get to see what happens if there are elements
+     * in the list. cur will point directly to the item located
+     * behind the index. 
+     */
+      } else {
+        /* three steps, first set new_ptr's next to the current item,
+         * this way no information is destroyed but a reference is produced
+         * second, set the next pointer of the item  previously preceding cur
+         * to point to the new item
+         * third set curs previous pointer to the new item.
+         */
+        Double_node *cur = find ( index );
+        new_ptr->next = cur;
+        cur->prev->next = new_ptr;
+        cur->prev = new_ptr;
+      }
     }
+  }
 }
 
 void Double_list::remove ( int index )
 {
-    if (( index < 1 ) || ( index > new_length )) {
-        throw list_index_out_of_range_exception(
-                "List Index Out of Range Exception: the index you provided is out of range");
-    } else {
+  if (( index < 1 ) || ( index > new_length )) {
+    throw list_index_out_of_range_exception(
+      "List Index Out of Range Exception: the index you provided is out of range");
+  } else {
 
 
 
