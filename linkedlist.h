737a1ec84aa07918922bee85bda890c7aa9160f3
derwolfe <chriswwolfe@gmail.com>
2011-09-26 22:52:22 -0500

    remove being tested

--- linkedlist.h
+++ linkedlist.h
@@ -3,6 +3,27 @@
 typedef int list_item_type;
 // THIS IS WRONG - you need a template, not a typedef
 
+    
+/* 
+ * the actual node in the doubly linked list
+ * item of list_item_type is an item in the list
+ * and prev is a pointer to the next node
+ *
+ * this is where you could feed in the template for item.
+ */
+struct Double_node {
+  list_item_type  item;
+  
+  /* 
+   * pointers to the previous and next nodes, 
+   * this is the distinguishing feature of the doubly
+   * linked list
+   */
+  Double_node   *prev;
+  Double_node   *next;
+};
+    
+
 class Double_list 
 {
   public:
@@ -16,7 +37,7 @@ class Double_list
     
     Double_list ( const Double_list& a_list );
     
-  /* destructor - basically a tail ended pop */
+    /* destructor - basically a tail ended pop */
     ~Double_list ();
 
     /* checks if the node is empty */
@@ -25,13 +46,11 @@ class Double_list
     /* returns the length */
     int get_length () const;
 
-//    Double_node *Double_list::find ( list_item_type& data_item ) const;
-
     /* adds the item to the end of the list.*/
     void item_add ( list_item_type new_item );
 
     /* finds then remove the  node */
-//    void remove ( list_item_type data_item );
+    void remove ( list_item_type data_item );
    
     /* pop function - tail end pop */
     void pop ();
@@ -40,26 +59,11 @@ class Double_list
      */
  //   friend std::ostream& operator<<(std::ostream &os, Double_list::Double_node &in_node);
     void print ();
+
+    Double_node *retrieve ( list_item_type data_item ) const;
   
   private:
-    
-    /* 
-     * the actual node in the doubly linked list
-     * item of list_item_type is an item in the list
-     * and prev is a pointer to the next node
-     */
-    struct Double_node {
-      list_item_type  item;
-      
-      /* 
-       * pointers to the previous and next nodes, 
-       * this is the distinguishing feature of the doubly
-       * linked list
-       */
-      Double_node   *prev;
-      Double_node   *next;
-    };
-    
+
     /* 
      * the total size of the list
      * and and a pointer to the node
@@ -67,7 +71,6 @@ class Double_list
     int size;
     Double_node   *head;
     Double_node   *tail;
-//    
     Double_node   *find ( list_item_type data_item ) const;
 };
 
