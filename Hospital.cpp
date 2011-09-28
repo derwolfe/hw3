78d0cb7da52080c59b006add8046a02091127ab0
derwolfe <chriswwolfe@gmail.com>
2011-09-28 14:32:54 -0500

    imped more of the doctor class, still not sure, i understand how to use the template

--- Hospital.cpp
+++ Hospital.cpp
@@ -1,40 +1,56 @@
-
+typedef Node_item_type Doctor
 
 Hospital::Hospital ( const &in_name )
 {
   name = in_name;
-  doctors = Double_list ( );
-  patients = Double_list ( );
+  doctors = new Double_list ( );
+  patients = new Double_list ( );
 }
 
-void Hospital::add_patient(string doctor_firstname, 
+void Hospital::add_patient (string doctor_firstname, 
         string doctor_lastname, 
-        Patient *patient);
+        Patient *patient );
 /* find the doctor, add the patient to the doctor
  */
 {
-  Doctor *target = doctors.retrieve ( doctor_firstname, doctor_lastname );
-
+  Doctor *doctor = doctors.retrieve ( doctor_firstname, doctor_lastname );
+  doctor.add_patient ( *patient );
+  doctor = NULL;
 }
 
-    /* search for the Doctor then the patient
+    /* search for the Doctor then the patient, using the method defined
+     * by your linkedlist template
      */    
-void Hospital::delete_patient(string doctor_firstname, 
+void Hospital::delete_patient ( string doctor_firstname, 
         string doctor_lastname, 
         string firstname, 
-        string lastname);
+        string lastname );
 {
-  
-    
-    Patient* search_patient(string doctor_firstname, 
+  Doctor *doctor = doctors.retrieve ( doctor_firstname, doctor_lastname );
+  doctor.delete_patient ( firstname, lastname );
+}
+
+Patient* Hospital::search_patient ( string doctor_firstname, 
         string doctor_lastname, 
         string firstname, 
-        string lastname);
+        string lastname );
+{
+  Doctor *doctor = doctors.retrieve ( doctor_firstname, doctor_lastname );
+  Patient *patient = doctor.search_patient ( firstname, lastname );
+  return patient;
+}
     
-    void hire_doctor(string doctor_firstname, string doctor_lastname);
+void Hospital::hire_doctor (string doctor_firstname, string doctor_lastname);
+{
+  doctors.item_add ( doctor_firstname, doctor_lastname );
+}
     
-    void fire_doctor(string doctor_firstname, string doctor_lastname);
-    
+void Hospital::fire_doctori (string doctor_firstname, string doctor_lastname);
+{
+  Doctor *target = doctors.search_doctor (doctor_firstname, doctor_lastname );
+  doctors.remove ( target );
+  target = NULL;
+}
     Doctor*  search_doctor(string doctor_firstname, string doctor_lastname);
 
     friend ostream& operator<<( ostream &os, Hospital &hospital);
