9738501b3b43903ba3eff5ae1b554123d88d155b
derwolfe <chriswwolfe@gmail.com>
2011-10-02 22:32:02 -0500

    having problems getting item add to work. the copy constructor works, but i am not calling it correctly, I think.

--- Doctor.cpp
+++ Doctor.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 #include "linkedlist_template.h"
-#include "Doctor.h"
 #include "Patient.h"
+#include "Doctor.h"
 
 /* 
  * construct a class of doctor, that has a list of patients
@@ -18,45 +18,57 @@ Doctor::Doctor ( const string &doctor_first_name,
 {
   first_name  = doctor_first_name;
   last_name   = doctor_lastname;
-  patients    = Double_list <Patient> () ;
+  patients    = Double_list<Patient> ();
 }
 
-void Doctor::add_patient <Patient>  ( Patient &in_patient )
-{
-  patients.item_add ( in_patient )
+void Doctor::add_patient ( const Patient &in_patient )
+{/* problematic: the double list template should take the patient
+  * object and just shove it inside of a new double_node structure
+  * for whatever reason patients.item_add is calling a Patient::
+  * Patient constructor
+  */
+  // copy patient, add
+  Patient *pat = new Patient ( in_patient ); 
+  cout << *pat << endl;
 }
 /* find the patient node, delete it. You will need to use string 
  * functions to compare the data. You basically need to compare
  * every patient to the data fed in.
  */
-void Doctor::delete_patient <Patient> ( string first_name, string last_name )
-{
-  Patient *target = search_patient ( first_name, last_name ); 
-  patients.remove ( target );
-  delete target;
-  target = NULL;
-}
+//void Doctor::delete_patient ( string first_name, string last_name )
+//{
+//  Double_node <Patient>* patient = patients.retrieve ( first_name, last_name ); 
+//  patients.remove ( patient );
+//  delete patient;
+//  patient = NULL;
+//}
+//
+///* traverse the patient nodes and check to see if
+// * the content is equal. This shoud basically be a MATCH function,
+// * matching the input to the data_item.
+// */
+//Double_node<Patient>* Doctor::search_patient ( string first_name, string last_name ) 
+//{
+//  return ( patients.retrieve ( first_name, last_name ));
+//}
 
-/* you must traverse the patient nodes and check to see if
- * the content is equal. This shoud basically be a MATCH function,
- * matching the input to the data_item.
- */
-Patient* Doctor::search_patient <Patient> ( string first_name, string last_name ) 
+std::ostream& operator<<( std::ostream &os, Doctor &in_doctor )
 {
-  return ( patients.search ( first_name, last_name ));
-}
-
-friend ostream& operator<<( ostream &os, Doctor &doctor )
-{
-  os << " Doctor: " << doctor.first_name << "" << doctor.last_name  << endl;
-  os << "   List of patients:"                                      << endl;
-  os << " --  " << doctor.patients                                  << endl;
+  /* this isn't working because of the in_doctor.patients line
+   * the goal is to take doctor, and use its overloaded patient
+   * operator to print out the each patient. I would think calling
+   * patient would call its operator. But there must need to be some
+   * loop to cycle through each patient?
+   */
+  os << " Doctor: " << in_doctor.first_name 
+    << " " << in_doctor.last_name << endl;
+  os << "   List of patients:" << endl;
+    in_doctor.patients.print_items( os );
   os << endl;
   return os; 
 }
 
-void Doctor::print ( ostream &os )
+void Doctor::print ( std::ostream &os )
 {
   os << this << endl;
-}
-  
+} 
--- Doctor.h
+++ Doctor.h
@@ -4,19 +4,19 @@ class Doctor
     Doctor ( const string &doctor_first_name,
         const string &doctor_lastname );
 
-    void add_patient ( Patient &in_patient );
+    void add_patient ( const Patient &in_patient );
 
     void delete_patient ( string first_name, string lastname );
 
-    Patient* search_patient ( string first_name, string lastname );
-    
-    friend ostream& operator<<( ostream &os, Doctor &doctor);
+    Double_node<Patient>* search_patient ( string first_name, string lastname );
 
-    void print ( ostream &os );
+    friend std::ostream& operator<<( std::ostream &os, Doctor &in_doctor);
+    
+    void print ( std::ostream &os );
 
   private:
-    string        first_name;
-    string        last_name;
-    Double_list   *patients;
-}
+    string                  first_name;
+    string                  last_name;
+    Double_list<Patient>    patients;
+};
 
